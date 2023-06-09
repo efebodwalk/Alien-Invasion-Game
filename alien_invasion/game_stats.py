b91e0fb0a404ea8b6a1e5e8821d518d224315fad
Efeosa Bodunrin <102677386+efebodwalk@users.noreply.github.com>
2023-06-09 19:21:02 +0100

    Add files via upload

---
+++ alien_invasion/game_stats.py
@@ -0,0 +1,20 @@
+class GameStats:
+	"""tracks the stats for alien invasion."""
+
+	def __init__(self,ai_game):
+		"""initialize stats."""
+		self.settings = ai_game.settings
+		self.reset_stats()
+
+		# high score should never be reset.
+		self.high_score = 0
+
+	def reset_stats(self):
+		"""initializes stats that can change during the game"""
+		self.ships_left = self.settings.ship_limit
+		self.score = 0
+		self.level = 1
+
+		# start alien invasion in an inactive state
+		self.game_active = False
+
